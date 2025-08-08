2c5ee927d5cd273481d73277a031986ffe9b4921
SsL27 <leovictori2005@gmail.com>
2025-08-08 02:11:47 -0600

    Configuracion de Capacitor
    Modificación de parámetros para exportar el proyecto en Android Studio

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.ferreteria.gest',
   appName: 'FerreteriaGestor',
   webDir: 'www'
 };
